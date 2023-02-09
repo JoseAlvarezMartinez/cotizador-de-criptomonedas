e6cec18699e7a6fc5e40c39520aef959c009a3aa
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-02-09 10:17:37 -0300

    -- estilos finalizados --

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import styled from "@emotion/styled";
+import ImagenCripto from "./img/imagen-criptos.png";
+import "./App.css";
+
+const Heading = styled.h1`
+  color:#fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top:80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after{
+    content:"";
+    width:100px;
+    height:6px;
+    background-color: #66A2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width:992px){
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    column-gap:2rem ;
+  }
+`;
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+function App() {
+  return <Contenedor>
+    <Imagen src={ImagenCripto} alt="Imagen de criptomonedas"/>
+    <Heading>Cotiza Criptomonedas al Instante</Heading>
+  </Contenedor>;
+}
+
+export default App;
